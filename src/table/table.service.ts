6fe677137b4a51c3249a4d1940a6014a3ee542ee
Bresolin <pettbresolin@hotmail.com>
2023-01-27 23:42:53 -0300

    feat(table): implement create table endpoint

--- src/table/table.service.ts
+++ src/table/table.service.ts
@@ -1,12 +1,19 @@
 import { Injectable } from '@nestjs/common';
 import { createTableDto } from './dto/create-table.dto';
+import { Table } from './entities/table.entity';
 
 @Injectable()
 export default class TableService {
+  tables: Table[] = [];
+
   findAll() {
     return 'Buscar todas as mesas';
   }
   create(createTableDto: createTableDto) {
-    return 'Criar uma mesa: ' + JSON.stringify(createTableDto);
+    const table: Table = { id: `random_id`, ...createTableDto };
+
+    this.tables.push(table);
+
+    return table;
   }
 }
