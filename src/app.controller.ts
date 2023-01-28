18d3b02b51600619c53706e33d961afc1d3ff6f5
Bresolin <pettbresolin@hotmail.com>
2023-01-28 00:51:57 -0300

    docs: implement swagger

--- src/app.controller.ts
+++ src/app.controller.ts
@@ -1,12 +1,15 @@
 import { Controller, Get } from '@nestjs/common';
+import { ApiTags } from '@nestjs/swagger/dist';
 import { AppService } from './app.service';
 
+@ApiTags('table')
+@ApiTags('status')
 @Controller()
 export class AppController {
   constructor(private readonly appService: AppService) {}
 
   @Get()
-  getHello(): string {
-    return this.appService.getHello();
+  getAppStatus(): string {
+    return this.appService.getAppStatus();
   }
 }
--- src/table/dto/create-table.dto.ts
+++ src/table/dto/create-table.dto.ts
@@ -1,3 +1,12 @@
+import { ApiProperty } from "@nestjs/swagger/dist/decorators";
+import { IsNumber, IsPositive } from "class-validator";
+
 export class createTableDto {
+    @IsNumber()
+    @IsPositive()
+    @ApiProperty({
+        description: 'Numero da mesa"',
+        example: 1,
+    })
     number: number;
 }
