82536c9d5b26398a799e48d656f753a448011d1c
Bresolin <pettbresolin@hotmail.com>
2023-02-13 20:50:12 -0300

    feat(auth): generate resource

--- src/user/user.service.ts
+++ src/user/user.service.ts
@@ -1,14 +1,14 @@
 import {
   BadRequestException,
   Injectable,
-  NotFoundException
-} from "@nestjs/common";
-import * as bcrypt from "bcrypt";
-import { PrismaService } from "src/prisma/prisma.service";
-import { handleError } from "src/utils/handle-error.util";
-import { CreateUserDto } from "./dto/create-user.dto";
-import { UpdateUserDto } from "./dto/update-user.dto";
-import { User } from "./entities/user.entity";
+  NotFoundException,
+} from '@nestjs/common';
+import * as bcrypt from 'bcrypt';
+import { PrismaService } from 'src/prisma/prisma.service';
+import { handleError } from 'src/utils/handle-error.util';
+import { CreateUserDto } from './dto/create-user.dto';
+import { UpdateUserDto } from './dto/update-user.dto';
+import { User } from './entities/user.entity';
 
 @Injectable()
 export class UserService {
@@ -19,21 +19,21 @@ export class UserService {
     password: false,
     image: true,
     createdAt: true,
-    updatedAt: true
+    updatedAt: true,
   };
 
   constructor(private readonly prisma: PrismaService) {}
 
   findAll(): Promise<User[]> {
     return this.prisma.user.findMany({
-      select: this.userSelect
+      select: this.userSelect,
     });
   }
 
   async findById(id: string): Promise<User> {
     const record = await this.prisma.user.findUnique({
       where: { id },
-      select: this.userSelect
+      select: this.userSelect,
     });
 
     if (!record) {
@@ -49,20 +49,20 @@ export class UserService {
 
   async create(dto: CreateUserDto): Promise<User> {
     if (dto.password != dto.confirmPassword) {
-      throw new BadRequestException("As senhas informadas não são iguais.");
+      throw new BadRequestException('As senhas informadas não são iguais.');
     }
 
     delete dto.confirmPassword;
 
     const data: User = {
       ...dto,
-      password: await bcrypt.hash(dto.password, 10)
+      password: await bcrypt.hash(dto.password, 10),
     };
 
     return this.prisma.user
       .create({
         data,
-        select: this.userSelect
+        select: this.userSelect,
       })
       .catch(handleError);
   }
@@ -71,8 +71,8 @@ export class UserService {
     await this.findById(id);
 
     if (dto.password) {
-      if (dto.password != dto.confirmPassword) {
-        throw new BadRequestException("As senhas informadas não são iguais.");
+      if (dto.password !== dto.confirmPassword) {
+        throw new BadRequestException('As senhas informadas não são iguais.');
       }
     }
 
@@ -88,7 +88,7 @@ export class UserService {
       .update({
         where: { id },
         data,
-        select: this.userSelect
+        select: this.userSelect,
       })
       .catch(handleError);
   }
