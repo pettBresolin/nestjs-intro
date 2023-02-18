4d91ee881286cfcd5b005ed78f9a880121e1153b
Bresolin <pettbresolin@hotmail.com>
2023-02-18 17:25:05 -0300

    chore(app): improve status  endpoint

--- src/main.ts
+++ src/main.ts
@@ -1,14 +1,17 @@
 import { ValidationPipe } from '@nestjs/common';
 import { NestFactory } from '@nestjs/core';
+import { NestExpressApplication } from '@nestjs/platform-express';
 import { DocumentBuilder, SwaggerModule } from '@nestjs/swagger';
 import { AppModule } from './app.module';
 
 async function bootstrap() {
-  const app = await NestFactory.create(AppModule, {
+  const app = await NestFactory.create<NestExpressApplication>(AppModule, {
     cors: {
       origin: 'http://127.0.0.1:8081',
     },
   });
+
+  app.set('trust proxy', 1);
 
   app.useGlobalPipes(new ValidationPipe());
 
